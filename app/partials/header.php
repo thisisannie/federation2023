ef45ecb30348be44f16c71f63e0739772ea25246
Annie Kyles <anniek@federation.net.nz>
2023-02-28 17:07:05 +1300

    Initial header and styling

--- app/partials/header.php
+++ app/partials/header.php
@@ -6,6 +6,23 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Federation</title>
 	<link rel="stylesheet" href="<?=HTML_ROOT;?>/css/bootstrap-grid.css">
+	<link rel="stylesheet" href="<?=HTML_ROOT;?>/css/styles.css">
 </head>
 <body>
+	<header class="container">
+		<div id="logo-container">
+			<a href="<?=HTML_ROOT;?>">
+				<img src="<?=HTML_ROOT;?>/images/brand/logo-282x40.png" alt="Federation" class="logo">
+			</a>
+		</div>
+		<nav>
+			<div class="nav-item"><a href="/work">Work</a></div>
+			<div class="nav-item has-child"><a href="/about">About</a>
+				<div class="nav-sub-items">
+					<div class="nav-sub-item"><a href="people">People</a></div>
+				</div>
+			</div>
+			<div class="nav-item"><a href="/work">Contact</a></div>
+		</nav>
+	</header>
 	
