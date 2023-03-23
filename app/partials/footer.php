6db3852b8ecdcb1c7cc3d1a1ce7d771903a593b4
Annie Kyles <anniek@federation.net.nz>
2023-03-23 16:39:38 +1300

    Rearanged file structure to tidy assets

--- app/partials/footer.php
+++ app/partials/footer.php
@@ -1,4 +1,4 @@
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
- <script src="<?=HTML_ROOT;?>/js/scripts.js"></script>
+ <script src="<?=HTML_ROOT;?>/assets/js/scripts.js"></script>
 </body>
 </html>
--- app/partials/header.php
+++ app/partials/header.php
@@ -5,14 +5,14 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Federation</title>
-	<link rel="stylesheet" href="<?=HTML_ROOT;?>/css/bootstrap-grid.css">
-	<link rel="stylesheet" href="<?=HTML_ROOT;?>/css/styles.css">
+	<link rel="stylesheet" href="<?=HTML_ROOT;?>/assets/css/bootstrap-grid.css">
+	<link rel="stylesheet" href="<?=HTML_ROOT;?>/assets/css/styles.css">
 </head>
 <body>
 	<header>
 		<div id="logo-container">
 			<a href="<?=HTML_ROOT;?>">
-				<img src="<?=HTML_ROOT;?>/images/brand/logo-federation.svg" alt="Federation" class="logo">
+				<img src="<?=HTML_ROOT;?>/assets/images/brand/logo-federation.svg" alt="Federation" class="logo">
 			</a>
 		</div>
 		<div id="nav__mobile-toggle">
