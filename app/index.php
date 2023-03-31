dc7f67916fa2a67e4fe7119eee1cbec2f2f27d29
Annie Kyles <anniek@federation.net.nz>
2023-03-31 17:17:24 +1300

    Footer layout desktop and mobile

--- app/index.php
+++ app/index.php
@@ -2,33 +2,39 @@
   include 'config.php';
 	include ABSPATH . '/partials/header.php';
 ?>
-	<div id="hero--home" class="hero hero--home">
-			<video preload="auto" autoplay playsinline muted="muted" loop class="feature-img__bgvideo"><source src="<?=HTML_ROOT;?>/assets/videos/920x518_mobile.webm" type="video/webm"><source src="<?=HTML_ROOT;?>/assets/videos/920x518_mobile.mp4" type="video/mp4"><source src="<?=HTML_ROOT;?>/assets/videos/920x518_mobile.ogv" type="video/ogg"></video>
-			<span id="what1" class="what">What does success</span>
-			<span id="what2" class="what">look like to you?</span>
+<div id="hero--home" class="hero hero--home">
+		<!-- <video preload="auto" autoplay playsinline muted="muted" loop class="feature-img__bgvideo"><source src="<?=HTML_ROOT;?>/assets/videos/920x518_mobile.webm" type="video/webm"><source src="<?=HTML_ROOT;?>/assets/videos/920x518_mobile.mp4" type="video/mp4"><source src="<?=HTML_ROOT;?>/assets/videos/920x518_mobile.ogv" type="video/ogg"></video> -->
+		<span id="what1" class="what h1">What does success</span>
+		<span id="what2" class="what h1">look like to you?</span>
+</div>
+<section>
+	<div class="container container-wide">
+		<div class="row justify-content-center">
+			<div class="col col-12 col-md-8">
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+				<p>We’re Federation, an advertising agency for the new age. We create provocative ideas that live within advertising, entertainment and technology.</p>
+			</div>
+		</div>	
 	</div>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
-	<h2>lorem</h2>
+</section>
+
 <?php 
 	include ABSPATH . '/partials/footer.php';
 ?>
--- app/partials/footer.php
+++ app/partials/footer.php
@@ -1,4 +1,32 @@
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
- <script src="<?=HTML_ROOT;?>/assets/js/scripts.js"></script>
+	<footer>
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col col-12 col-md-10">
+					<div class="row">
+						<div class="col col-12 col-sm-6 col-md-3 footer-menu small caps">
+							<ul>
+								<li><a href="#">Work</a></li>
+								<li><a href="#">About</a></li>
+								<li><a href="#">Services</a></li>
+								<li><a href="#">Contact</a></li>
+							</ul>
+						</div>
+						<div class="col col-12 col-sm-6 col-md-3 address">					
+							<p class="small">
+							<strong>Federation</strong><br>
+							<a href="https://www.google.com/maps/place/Level+1%2F111+Wellesley+Street+West,+Auckland+CBD,+Auckland+1010/@-36.8489407,174.756497,17z/data=!3m1!4b1!4m6!3m5!1s0x6d0d47f1e6053cbf:0xb837e4a9991cb353!8m2!3d-36.8489407!4d174.7586857!16s%2Fg%2F11s2mr0tmg" target="_blank">Level 1, 111 Wellesly Street West<br>
+							Auckland, 1010<br>
+							New Zealand</a></p>
+						</div>
+						<div class="col col-12 col-md-6 connect">
+							<p class="small">Connect with us: <img src="<?=HTML_ROOT;?>/assets/images/utility/linkedin.svg" class="svg linkedin" alt="LinkedIn" title="LinkedIn"></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+	<script src="<?=HTML_ROOT;?>/assets/js/scripts.js"></script>
 </body>
 </html>
