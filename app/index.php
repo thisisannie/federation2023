dae64e1e55b25bb6a8ba5b1a0c66a365772a7723
Annie Kyles <anniek@federation.net.nz>
2023-04-04 11:00:24 +1200

    Mobile h1

--- app/index.php
+++ app/index.php
@@ -4,8 +4,11 @@
 ?>
 <div id="hero--home" class="hero hero--home">
 		<!-- <video preload="auto" autoplay playsinline muted="muted" loop class="feature-img__bgvideo"><source src="<?=HTML_ROOT;?>/assets/videos/920x518_mobile.webm" type="video/webm"><source src="<?=HTML_ROOT;?>/assets/videos/920x518_mobile.mp4" type="video/mp4"><source src="<?=HTML_ROOT;?>/assets/videos/920x518_mobile.ogv" type="video/ogg"></video> -->
-		<span id="what1" class="what h1">What does success</span>
-		<span id="what2" class="what h1">look like to you?</span>
+		<span id="what1" class="what h1 d-none d-md-inline-block">What does success</span>
+		<span id="what2" class="what h1 d-none d-md-inline-block">look like to you?</span>
+</div>
+<div class="container">
+	<h1 class="d-md-none">What does success look like to you?</h1>
 </div>
 <section>
 	<div class="container container-wide">
