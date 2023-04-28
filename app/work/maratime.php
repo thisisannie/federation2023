ed0d1ecf71f25ff94387bb0915ff791540dfe44c
Annie Kyles <anniek@federation.net.nz>
2023-04-28 12:40:02 +1200

    Card layout and card layout text tweaks

--- app/work/maratime.php
+++ app/work/maratime.php
@@ -84,6 +84,22 @@
 	</section>
 </div>
 
+<div class="section-next cards">
+	<a href="<?=HTML_ROOT;?>/work/<?= $slug ?>.php" class="card card--next">
+		<div class="card__img">
+			<img src="<?=HTML_ROOT;?>/assets/images/work/maratime/thumb-420x680.webp" alt="placeholder" class="">
+		</div>
+		<div class="text">
+			<span class="card__header--sm">Maratime NZ</span>
+			<span class="card__header--lg">Kia Mataara</span>
+			<div class="labels">
+				<span class="label">Brand</span>
+				<span class="label">OOH</span>
+			</div>
+			<div class="button button--spot"><img class="svg" src="<?=HTML_ROOT;?>/assets/images/utility/arrow-right.svg" alt=">"></div>
+		</div>
+	</a>
+</div>
 <?php
 	include ABSPATH . '/partials/footer.php';
 ?>
