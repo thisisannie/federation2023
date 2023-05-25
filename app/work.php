fb110894945359f661c4e0259d9beeefa984acc1
Annie Kyles <anniek@federation.net.nz>
2023-05-25 18:24:24 +1200

    Gutter. Work hero image.

--- app/work.php
+++ app/work.php
@@ -14,8 +14,7 @@
 	include ABSPATH . '/partials/header.php';
 ?>
 <div class="hero">
-	<!-- <video preload="auto" autoplay playsinline muted="muted" loop class="feature-img__bgvideo"><source src="<?=HTML_ROOT;?>/assets/videos/920x518_mobile.webm" type="video/webm"><source src="<?=HTML_ROOT;?>/assets/videos/920x518_mobile.mp4" type="video/mp4"><source src="<?=HTML_ROOT;?>/assets/videos/920x518_mobile.ogv" type="video/ogg"></video> -->
-	<img src="<?=HTML_ROOT;?>/assets/images/placeholders/video-placeholder.webp" alt="" class="feature-img">
+	<img src="<?=HTML_ROOT;?>/assets/images/work/video-still.webp" alt="" class="feature-img">
 </div>
 <section>
 	<div class="container container-wide">
@@ -23,14 +22,15 @@
 			<div class="col col-12 col-md-8 col-lg-6">
 				<h1>Our Work</h1>
 				<p class="intro">Building brands, engaging people, making tills ring. Here are some of our most talked-about and effective advertising campaigns.</p>
-				<div id="work-filters">
+				<!-- <div id="work-filters">
 					<h5 class="filter-by">Filter by service:</h5>
 					<a id="filter--brand" class="label">Brand</a>
 					<a id="filter--social" class="label">Social</a>
 					<a id="filter--creative" class="label">Creative</a>
 					<a id="filter--website" class="label">Website</a>
 					<a id="filter--strategy" class="label">Strategy</a>
-				</div>
+				</div> -->
+				<div class="gutter"></div>
 			</div>
 		</div>
 		<div id="work-cards" class="cards">
