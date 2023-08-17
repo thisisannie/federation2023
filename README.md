ff02d5e4d3269167eaf5157bde6f88cd5247a544
Annie Kyles <annakyles@gmail.com>
2023-08-18 10:10:34 +1200

    Added setup notes, changed HTML_ROOT

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### About ###
 This is a new website for Federation, advertising agency in New Zealand. 
 Inhouse design by Gene Wheaton, development by Annie Kyles. 
+
+Setup notes:
+PHP 8.2.4
+Set HTML_ROOT in app/config.php to match the environment path
--- app/config.php
+++ app/config.php
@@ -2,14 +2,14 @@
 header('Cache-Control: max-age=31536000');
 
 define('ABSPATH', dirname(__FILE__));
-define('HTML_ROOT', '/federation2023/app');
+define('HTML_ROOT', 'http://federation2023.localhost');
 
-function write_to_console($data) {
- $console = $data;
- if (is_array($console))
- $console = implode(',', $console);
+// function write_to_console($data) {
+//  $console = $data;
+//  if (is_array($console))
+//  $console = implode(',', $console);
 
- echo "<script>console.log('" . $console . "' );</script>";
-}
-write_to_console('ABSPATH = ' . ABSPATH);
-write_to_console('HTML_ROOT = ' . HTML_ROOT);
+//  echo "<script>console.log('" . $console . "' );</script>";
+// }
+// write_to_console('ABSPATH = ' . ABSPATH);
+// write_to_console('HTML_ROOT = ' . HTML_ROOT);
