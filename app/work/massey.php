11a41c1adff382f53547f61ab2f88b0b094fcd5c
Annie Kyles <annakyles@gmail.com>
2023-08-21 23:18:13 +1200

    Typo fix

--- app/work/massey.php
+++ app/work/massey.php
@@ -86,7 +86,7 @@
 				<h5>Solution</h5>
 				<p>The tertiary sector is rife with images of students gazing misty-eyed into the future. But the truth is that student life can be difficult and even overwhelming. We embraced the irrefutable fact that only mahi gets results. Success comes from a thousand crazy, exhausting, exhilarating moments. In a visually distinctive way, and with a whole lot of personality, we brought to life one student insight after another, showing how Massey walks beside their students to help them succeed.</p>
 				<h5>Results</h5>
-				<p>While the competition waved their chequebooks, we increased awareness and conversion on a small budget. Our new platform worked its socks off. Massey went from the number five spot to the most preferred University in New Zealand, in just three months. Job done.</p>
+				<p>While the competition waved their cheque books, we increased awareness and conversion on a small budget. Our new platform worked its socks off. Massey went from the number five spot to the most preferred University in New Zealand, in just three months. Job done.</p>
 			</div>
 		</div>
 	</div>
